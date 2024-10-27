95d294f45f03c42c280956f3f52294b37134152a
Sergiu <82155181+SergiuPetrut@users.noreply.github.com>
2024-10-27 23:42:00 +0200

    Added OpenMP project!

---
+++ build/CMakeFiles/parallel.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parallel.dir/parallel.c.o"
+  "CMakeFiles/parallel.dir/parallel.c.o.d"
+  "parallel"
+  "parallel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/parallel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
